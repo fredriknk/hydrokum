42bbe4bb84c708c8e55a92b3242870a8fc289a94
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2023-08-25 13:37:27 +0200

    Improved DB

--- database.py
+++ database.py
@@ -3,69 +3,65 @@ import socket
 import time
 import sqlite3
 from sqlite3 import Error
+from typing import List, Tuple, Optional
 
-# Your socket and database configuration here...
-HOST = '10.0.20.3'  # Replace with your host
-PORT = 51020  # Replace with your port
 
-def create_connection():
-    conn = None;
-    try:
-        conn = sqlite3.connect('my_database.sqlite') # Creates SQLite database file if it doesn't exist
-        return conn
-    except Error as e:
-        print(e)
+class Database:
+    def __init__(self, db_name: str):
+        self.db_name = db_name
 
-    return conn
+    def create_connection(self) -> Optional[sqlite3.Connection]:
+        try:
+            conn = sqlite3.connect(self.db_name)
+            return conn
+        except Error as e:
+            print(e)
+        return None
 
-def create_table():
-    conn = create_connection()
-    try:
-        conn.execute('''CREATE TABLE IF NOT EXISTS data
-                     (time text, N2O_ppm real, CO2_ppm real, CH4_ppm real, NH3_ppb real)''')
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
+    def create_table(self) -> None:
+        conn = self.create_connection()
+        with conn:
+            conn.execute('''CREATE TABLE IF NOT EXISTS data
+                         (time text, N2O_ppm real, CO2_ppm real, CH4_ppm real, NH3_ppb real)''')
 
-def insert_data(data):
-    conn = create_connection()
-    try:
-        conn.execute("INSERT INTO data (time, N2O_ppm, CO2_ppm, CH4_ppm, NH3_ppb) VALUES (?, ?, ?, ?, ?)", data)
-        conn.commit()
-        conn.close()
-    except Error as e:
-        print(e)
+    def insert_data(self, data: List) -> None:
+        conn = self.create_connection()
+        with conn:
+            conn.execute("INSERT INTO data (time, N2O_ppm, CO2_ppm, CH4_ppm, NH3_ppb) VALUES (?, ?, ?, ?, ?)", data)
 
-def query_data(last_plotted_time=None, lim = 1000):
-    conn = create_connection()
-    cur = conn.cursor()
+    def query_data(self, last_plotted_time: Optional[str] = None, lim: int = 1000) -> List[Tuple]:
+        conn = self.create_connection()
+        with conn:
+            cur = conn.cursor()
+            if last_plotted_time is None:
+                cur.execute(f"SELECT * FROM data ORDER BY time DESC LIMIT {lim}")
+            else:
+                cur.execute("SELECT * FROM data WHERE time > ? ORDER BY time ASC", (last_plotted_time,))
+            return cur.fetchall()
 
-    if last_plotted_time is None:
-        cur.execute(f"SELECT * FROM data ORDER BY time DESC LIMIT {lim}")
-    else:
-        cur.execute("SELECT * FROM data WHERE time > ? ORDER BY time ASC", (last_plotted_time,))
 
-    rows = cur.fetchall()
-    conn.close()
+def main():
+    HOST = '10.0.20.3'
+    PORT = 51020
+    db = Database('my_database.sqlite')
+    db.create_table()
 
-    return rows
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(b"_Meas_GetConc\r")
+            data = s.recv(1024).decode('utf-8')
 
-# Create table if it doesn't exist
-create_table()
+        values = data.strip().split(';')
+        values_to_plot = [float(values[2]), float(values[7]), float(values[9]), float(values[12])]
+        print(values)
 
-# Inside your infinite loop
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b"_Meas_GetConc\r")
-        data = s.recv(1024).decode('utf-8')
+        # Insert new data into the database
+        db.insert_data([str(datetime.datetime.now())] + values_to_plot)
 
-    values = data.strip().split(';')
-    values_to_plot = [float(values[2]), float(values[7]), float(values[9]), float(values[12])]
-    print(values)
-    # Insert new data into the database
-    insert_data([datetime.datetime.now()] + values_to_plot)
+        # Sleep for 5 seconds
+        time.sleep(5)
 
-    # Sleep for one second
-    time.sleep(5)
+
+if __name__ == "__main__":
+    main()
